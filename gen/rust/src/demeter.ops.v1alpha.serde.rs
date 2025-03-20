c22f50b9f2b9c69c8e68535394f76753a3b1f1d2
paulobressan <paulo.bressan@outlook.com>
2025-03-20 15:32:03 -0300

    feat: added fetch usage clusters and add new usage filter

--- gen/rust/src/demeter.ops.v1alpha.serde.rs
+++ gen/rust/src/demeter.ops.v1alpha.serde.rs
@@ -3848,7 +3848,7 @@ impl<'de> serde::Deserialize<'de> for FetchResourcesResponse {
         deserializer.deserialize_struct("demeter.ops.v1alpha.FetchResourcesResponse", FIELDS, GeneratedVisitor)
     }
 }
-impl serde::Serialize for FetchUsageReportRequest {
+impl serde::Serialize for FetchUsageClusterRequest {
     #[allow(deprecated)]
     fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
     where
@@ -3865,7 +3865,7 @@ impl serde::Serialize for FetchUsageReportRequest {
         if self.page_size.is_some() {
             len += 1;
         }
-        let mut struct_ser = serializer.serialize_struct("demeter.ops.v1alpha.FetchUsageReportRequest", len)?;
+        let mut struct_ser = serializer.serialize_struct("demeter.ops.v1alpha.FetchUsageClusterRequest", len)?;
         if !self.project_id.is_empty() {
             struct_ser.serialize_field("projectId", &self.project_id)?;
         }
@@ -3878,7 +3878,7 @@ impl serde::Serialize for FetchUsageReportRequest {
         struct_ser.end()
     }
 }
-impl<'de> serde::Deserialize<'de> for FetchUsageReportRequest {
+impl<'de> serde::Deserialize<'de> for FetchUsageClusterRequest {
     #[allow(deprecated)]
     fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
     where
@@ -3930,13 +3930,13 @@ impl<'de> serde::Deserialize<'de> for FetchUsageReportRequest {
         }
         struct GeneratedVisitor;
         impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
-            type Value = FetchUsageReportRequest;
+            type Value = FetchUsageClusterRequest;
 
             fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                formatter.write_str("struct demeter.ops.v1alpha.FetchUsageReportRequest")
+                formatter.write_str("struct demeter.ops.v1alpha.FetchUsageClusterRequest")
             }
 
-            fn visit_map<V>(self, mut map_: V) -> std::result::Result<FetchUsageReportRequest, V::Error>
+            fn visit_map<V>(self, mut map_: V) -> std::result::Result<FetchUsageClusterRequest, V::Error>
                 where
                     V: serde::de::MapAccess<'de>,
             {
@@ -3969,10 +3969,250 @@ impl<'de> serde::Deserialize<'de> for FetchUsageReportRequest {
                         }
                     }
                 }
+                Ok(FetchUsageClusterRequest {
+                    project_id: project_id__.unwrap_or_default(),
+                    page: page__,
+                    page_size: page_size__,
+                })
+            }
+        }
+        deserializer.deserialize_struct("demeter.ops.v1alpha.FetchUsageClusterRequest", FIELDS, GeneratedVisitor)
+    }
+}
+impl serde::Serialize for FetchUsageClusterResponse {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if !self.clusters.is_empty() {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("demeter.ops.v1alpha.FetchUsageClusterResponse", len)?;
+        if !self.clusters.is_empty() {
+            struct_ser.serialize_field("clusters", &self.clusters)?;
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for FetchUsageClusterResponse {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "clusters",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            Clusters,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> std::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "clusters" => Ok(GeneratedField::Clusters),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = FetchUsageClusterResponse;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("struct demeter.ops.v1alpha.FetchUsageClusterResponse")
+            }
+
+            fn visit_map<V>(self, mut map_: V) -> std::result::Result<FetchUsageClusterResponse, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut clusters__ = None;
+                while let Some(k) = map_.next_key()? {
+                    match k {
+                        GeneratedField::Clusters => {
+                            if clusters__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("clusters"));
+                            }
+                            clusters__ = Some(map_.next_value()?);
+                        }
+                    }
+                }
+                Ok(FetchUsageClusterResponse {
+                    clusters: clusters__.unwrap_or_default(),
+                })
+            }
+        }
+        deserializer.deserialize_struct("demeter.ops.v1alpha.FetchUsageClusterResponse", FIELDS, GeneratedVisitor)
+    }
+}
+impl serde::Serialize for FetchUsageReportRequest {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if !self.project_id.is_empty() {
+            len += 1;
+        }
+        if self.page.is_some() {
+            len += 1;
+        }
+        if self.page_size.is_some() {
+            len += 1;
+        }
+        if self.cluster_id.is_some() {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("demeter.ops.v1alpha.FetchUsageReportRequest", len)?;
+        if !self.project_id.is_empty() {
+            struct_ser.serialize_field("projectId", &self.project_id)?;
+        }
+        if let Some(v) = self.page.as_ref() {
+            struct_ser.serialize_field("page", v)?;
+        }
+        if let Some(v) = self.page_size.as_ref() {
+            struct_ser.serialize_field("pageSize", v)?;
+        }
+        if let Some(v) = self.cluster_id.as_ref() {
+            struct_ser.serialize_field("clusterId", v)?;
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for FetchUsageReportRequest {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "project_id",
+            "projectId",
+            "page",
+            "page_size",
+            "pageSize",
+            "cluster_id",
+            "clusterId",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            ProjectId,
+            Page,
+            PageSize,
+            ClusterId,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> std::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "projectId" | "project_id" => Ok(GeneratedField::ProjectId),
+                            "page" => Ok(GeneratedField::Page),
+                            "pageSize" | "page_size" => Ok(GeneratedField::PageSize),
+                            "clusterId" | "cluster_id" => Ok(GeneratedField::ClusterId),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = FetchUsageReportRequest;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("struct demeter.ops.v1alpha.FetchUsageReportRequest")
+            }
+
+            fn visit_map<V>(self, mut map_: V) -> std::result::Result<FetchUsageReportRequest, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut project_id__ = None;
+                let mut page__ = None;
+                let mut page_size__ = None;
+                let mut cluster_id__ = None;
+                while let Some(k) = map_.next_key()? {
+                    match k {
+                        GeneratedField::ProjectId => {
+                            if project_id__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("projectId"));
+                            }
+                            project_id__ = Some(map_.next_value()?);
+                        }
+                        GeneratedField::Page => {
+                            if page__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("page"));
+                            }
+                            page__ = 
+                                map_.next_value::<::std::option::Option<::pbjson::private::NumberDeserialize<_>>>()?.map(|x| x.0)
+                            ;
+                        }
+                        GeneratedField::PageSize => {
+                            if page_size__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("pageSize"));
+                            }
+                            page_size__ = 
+                                map_.next_value::<::std::option::Option<::pbjson::private::NumberDeserialize<_>>>()?.map(|x| x.0)
+                            ;
+                        }
+                        GeneratedField::ClusterId => {
+                            if cluster_id__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("clusterId"));
+                            }
+                            cluster_id__ = map_.next_value()?;
+                        }
+                    }
+                }
                 Ok(FetchUsageReportRequest {
                     project_id: project_id__.unwrap_or_default(),
                     page: page__,
                     page_size: page_size__,
+                    cluster_id: cluster_id__,
                 })
             }
         }
--- gen/rust/src/demeter.ops.v1alpha.tonic.rs
+++ gen/rust/src/demeter.ops.v1alpha.tonic.rs
@@ -2786,6 +2786,37 @@ pub mod usage_service_client {
                 );
             self.inner.unary(req, path, codec).await
         }
+        ///
+        pub async fn fetch_usage_cluster(
+            &mut self,
+            request: impl tonic::IntoRequest<super::FetchUsageClusterRequest>,
+        ) -> std::result::Result<
+            tonic::Response<super::FetchUsageClusterResponse>,
+            tonic::Status,
+        > {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/demeter.ops.v1alpha.UsageService/FetchUsageCluster",
+            );
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(
+                    GrpcMethod::new(
+                        "demeter.ops.v1alpha.UsageService",
+                        "FetchUsageCluster",
+                    ),
+                );
+            self.inner.unary(req, path, codec).await
+        }
     }
 }
 /// Generated server implementations.
@@ -2801,6 +2832,14 @@ pub mod usage_service_server {
             request: tonic::Request<super::FetchUsageReportRequest>,
         ) -> std::result::Result<
             tonic::Response<super::FetchUsageReportResponse>,
+            tonic::Status,
+        >;
+        ///
+        async fn fetch_usage_cluster(
+            &self,
+            request: tonic::Request<super::FetchUsageClusterRequest>,
+        ) -> std::result::Result<
+            tonic::Response<super::FetchUsageClusterResponse>,
             tonic::Status,
         >;
     }
@@ -2916,6 +2955,53 @@ pub mod usage_service_server {
                     let fut = async move {
                         let inner = inner.0;
                         let method = FetchUsageReportSvc(inner);
+                        let codec = tonic::codec::ProstCodec::default();
+                        let mut grpc = tonic::server::Grpc::new(codec)
+                            .apply_compression_config(
+                                accept_compression_encodings,
+                                send_compression_encodings,
+                            )
+                            .apply_max_message_size_config(
+                                max_decoding_message_size,
+                                max_encoding_message_size,
+                            );
+                        let res = grpc.unary(method, req).await;
+                        Ok(res)
+                    };
+                    Box::pin(fut)
+                }
+                "/demeter.ops.v1alpha.UsageService/FetchUsageCluster" => {
+                    #[allow(non_camel_case_types)]
+                    struct FetchUsageClusterSvc<T: UsageService>(pub Arc<T>);
+                    impl<
+                        T: UsageService,
+                    > tonic::server::UnaryService<super::FetchUsageClusterRequest>
+                    for FetchUsageClusterSvc<T> {
+                        type Response = super::FetchUsageClusterResponse;
+                        type Future = BoxFuture<
+                            tonic::Response<Self::Response>,
+                            tonic::Status,
+                        >;
+                        fn call(
+                            &mut self,
+                            request: tonic::Request<super::FetchUsageClusterRequest>,
+                        ) -> Self::Future {
+                            let inner = Arc::clone(&self.0);
+                            let fut = async move {
+                                <T as UsageService>::fetch_usage_cluster(&inner, request)
+                                    .await
+                            };
+                            Box::pin(fut)
+                        }
+                    }
+                    let accept_compression_encodings = self.accept_compression_encodings;
+                    let send_compression_encodings = self.send_compression_encodings;
+                    let max_decoding_message_size = self.max_decoding_message_size;
+                    let max_encoding_message_size = self.max_encoding_message_size;
+                    let inner = self.inner.clone();
+                    let fut = async move {
+                        let inner = inner.0;
+                        let method = FetchUsageClusterSvc(inner);
                         let codec = tonic::codec::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
                             .apply_compression_config(
